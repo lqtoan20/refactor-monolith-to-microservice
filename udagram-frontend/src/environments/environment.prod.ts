713a4ef4e57872e6d8125bcd243d8af9f933743c
ToanLa <lqtoan20@gmail.com>
2023-03-30 21:04:59 +0700

    update host for fron-end app and rebuild travis

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram Service App",
+  apiHost:
+    "http://a11761ed232594d3b98ac14c5c27d013-716281456.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
